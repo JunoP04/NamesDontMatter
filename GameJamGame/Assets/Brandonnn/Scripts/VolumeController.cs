3df68d3753b6c5659a5dd468520c12d38879d178
Brandon Samaroo <samaroo.bran@gmail.com>
2020-02-01 14:31:11 -0500

    UI HUD

---
+++ GameJamGame/Assets/Brandonnn/Scripts/VolumeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Start()
+    {
+        slider.value = GameManager.gameManager.volume;
+    }
+
+    public void ChangeVolume()
+    {
+        GameManager.gameManager.volumeChange(slider.value);
+    }
+}
