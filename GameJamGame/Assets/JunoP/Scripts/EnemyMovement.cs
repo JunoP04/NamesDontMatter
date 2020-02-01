89eddb398d26287899fe5d9c7dbec30057c201b5
JunoP04 <50187932+JunoP04@users.noreply.github.com>
2020-02-01 00:25:44 -0500

    Unfinished shoot script

--- GameJamGame/Assets/JunoP/Scripts/EnemyMovement.cs
+++ GameJamGame/Assets/JunoP/Scripts/EnemyMovement.cs
@@ -6,6 +6,10 @@ public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed;
     public GameObject target;
+    public GameObject enemyShoot;
+    public GameObject enemy;
+    public GameObject player;
+    public bool canShoot = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,25 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+        float distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if(distance > 2.5)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+        }
+        else 
+        {
+            
+        }
+
+    }
+
+    public void ShootPlayer()
+    {
+        GameObject projectile = Instantiate(enemyShoot, enemy.transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = projectile.GetComponent<Rigidbody2D>();
+        rb2d.velocity = (player.transform.position - transform.position).normalized;
+        Destroy(projectile, 3.0f);
+        canShoot = false;
     }
 }
--- GameJamGame/Assets/JunoP/Scripts/ShipMovement.cs
+++ GameJamGame/Assets/JunoP/Scripts/ShipMovement.cs
@@ -19,5 +19,25 @@ public class ShipMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical") * moveSpeed;
         float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
         transform.Translate(moveHorizontal * Time.deltaTime, moveVertical * Time.deltaTime, 0);
+
+        // X axis
+        if (transform.position.x <= -10.0f)
+        {
+            transform.position = new Vector2(-10.0f, transform.position.y);
+        }
+        else if (transform.position.x >= 10.0f)
+        {
+            transform.position = new Vector2(10.0f, transform.position.y);
+        }
+
+        // Y axis
+        if (transform.position.y <= -4.5f)
+        {
+            transform.position = new Vector2(transform.position.x, -4.5f);
+        }
+        else if (transform.position.y >= 6.5f)
+        {
+            transform.position = new Vector2(transform.position.x, 6.5f);
+        }
     }
 }
