4935409e4ecdb7133e9679a8b55f2bf483109f68
JunoP04 <50187932+JunoP04@users.noreply.github.com>
2020-01-31 20:36:23 -0500

    Enemy Movement

---
+++ GameJamGame/Assets/JunoP/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ GameJamGame/Assets/JunoP/Scripts/ShipMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement of the ship
+        float moveVertical = Input.GetAxis("Vertical") * moveSpeed;
+        float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        transform.Translate(moveHorizontal * Time.deltaTime, moveVertical * Time.deltaTime, 0);
+    }
+}
